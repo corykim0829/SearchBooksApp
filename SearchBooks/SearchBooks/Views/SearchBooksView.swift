292ca90e3d12ee8c4fdfe9dc700fa4a1085fa316
corykim0829 <corykim0829@gmail.com>
2024-06-15 14:36:28 +0900

    feat: SearchBooksView 레이아웃 수정 및 뷰모델 처리

--- SearchBooks/SearchBooks/Views/SearchBooksView.swift
+++ SearchBooks/SearchBooks/Views/SearchBooksView.swift
@@ -9,24 +9,79 @@ import SwiftUI
 
 struct SearchBooksView: View {
   
-  @State private var searchKeyword: String = ""
+  @StateObject var viewModel = SearchBooksViewModel()
+  
+  @State var fetchedBooks : [Book] = []
   
   var body: some View {
+    
     NavigationView {
-      List {
-        Text("Hello, World!")
-        Text("Hello, World!")
-        Text("Hello, World!")
-        Text("Hello, World!")
-        Text("Hello, World!")
+      
+      ScrollView(.vertical) {
+        
+        VStack(spacing: 16) {
+          HStack(spacing: 10) {
+            Image(systemName: "magnifyingglass")
+            TextField("책을 검색하세요", text: $viewModel.searchKeyword)
+          }
+          .padding(.vertical, 10)
+          .padding(.horizontal)
+          .shadow(color: .black.opacity(0.06), radius: 5, x: 5, y: 5)
+          .shadow(color: .black.opacity(0.06), radius: 5, x: -5, y: -5)
+        }
+        .padding(.vertical, 16)
+        
+        if let books = viewModel.fetchedBooks {
+          
+          if books.isEmpty {
+            Text("결과가 없습니다.")
+              .padding(.top, 20)
+          } else {
+            LazyVStack(alignment: .leading) {
+              
+              ForEach(books, id: \.isbn13) { book in
+                NavigationLink {
+                  BookDetailView(book: book)
+                } label: {
+                  BookItemView(book: book)
+                }
+                .buttonStyle(PlainButtonStyle())
+              }
+            }
+            
+            // 다음 페이지 값 로딩뷰 처리
+            if viewModel.isFetchingNextPage {
+              ProgressView()
+            }
+            
+            GeometryReader { reader -> Color in
+              let minY = reader.frame(in: .global).minY
+              let height = UIScreen.main.bounds.height / 1.3
+//              print("minY", minY)
+//              print("height", height)
+              if minY < height {
+                DispatchQueue.main.async {
+                  self.viewModel.fetchNextPage()
+                }
+              }
+              
+              return Color.clear
+            }
+            
+          }
+          
+        } else {
+          if viewModel.searchKeyword != "" {
+            ProgressView()
+              .padding(.top, 20)
+          }
+          
+        }
+        
       }
-      .listStyle(.plain)
       .navigationTitle("IT 책 검색")
+      
     }
-    .searchable(
-      text: $searchKeyword,
-      placement: .navigationBarDrawer,
-      prompt: "책을 검색해보세요")
     
   }
 }
