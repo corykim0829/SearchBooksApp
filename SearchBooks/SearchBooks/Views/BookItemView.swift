4cf7ff2476c83c79ad4ea798e13db2d7cbbe7657
corykim0829 <corykim0829@gmail.com>
2024-06-15 14:31:53 +0900

    refactor: BookItemView 레이아웃 수정 및 바인딩 프로퍼티 래퍼 제거

--- SearchBooks/SearchBooks/Views/BookItemView.swift
+++ SearchBooks/SearchBooks/Views/BookItemView.swift
@@ -9,21 +9,26 @@ import SwiftUI
 
 struct BookItemView: View {
   
-  @Binding var book: Book
+  var book: Book
   
   var body: some View {
-    HStack(alignment: .top, spacing: 16) {
-      Rectangle()
-        .fill(.yellow)
-        .frame(width: 80, height: 100)
+    HStack(alignment: .top, spacing: 4) {
+      ImageView(urlString: book.image)
+      .frame(width: 100, height: 120)
       VStack(alignment: .leading) {
-        Text(book.title)
-          .font(.system(size: 20, weight: .semibold))
-        Text(book.subtitle)
+        VStack(alignment: .leading, spacing: 2) {
+          Text(book.title)
+            .font(.system(size: 20, weight: .semibold))
+            .frame(maxHeight: 28)
+          Text(book.subtitle)
+            .font(.system(size: 16, weight: .regular))
+            .frame(maxHeight: 48)
+        }
         Text(book.price)
           
       }
     }
+    .padding(.trailing, 20)
 
   }
   
@@ -34,7 +39,7 @@ struct BookItemView_Preview: PreviewProvider {
   static let sampleBook = Book(title: "책 제목입니다.", subtitle: "책에대한 설명입니다", isbn13: "", price: "$20", image: "", url: "")
   
   static var previews: some View {
-    BookItemView(book: .constant(sampleBook))
+    BookItemView(book: sampleBook)
   }
   
 }
